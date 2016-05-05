0147a49f5ab7c6ee634fa80eb17c01544333d157
Jon Hildebrand <snoopj@gmail.com>
2016-05-05 10:08:08 -0500

    1st attempt at some custom PoSH functions and SimpliVity REST APIs

---
+++ attempt1.ps1
@@ -0,0 +1,78 @@
+﻿Import-Module VMware.VimAutomation.Core
+
+Add-Type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+
+function Get-AccessToken()
+{
+    $url = "https://10.20.4.143/api/oauth/token"
+    $base64 = [Convert]::ToBase64String([System.Text.UTF8Encoding]::UTF8.GetBytes("simplivity:"))
+    $cred = Get-Credential
+    $username = $cred.UserName
+    $pass_word = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($cred.Password))
+    $body = @{username="$username";password="$pass_word";grant_type="password"}
+    $headers = @{}
+    $headers.Add("Authorization", "Basic $base64")
+    $headers.Add("Accept", "application/json")
+    $ret_token = Invoke-RestMethod -Uri $url -Headers $headers -Body $body -Method Post
+    $global:Token = $ret_token.access_token
+    #return $ret_token.access_token
+}
+
+function Get-OmniVM([string]$VMName)
+{
+    $url = "https://10.20.4.143/api/virtual_machines"
+    $header = @{}
+    $header.Add("Authorization", "Bearer $Token")
+    $header.Add("Accept", "application/json")
+    $body = @{}
+    $body.Add("show_optional_fields", "false")
+    $body.Add("name", "$VMName")
+    $result = Invoke-RestMethod -Uri $url -Headers $header -Body $body -Method Get
+    return $result.virtual_machines.id
+}
+
+function Clone-OmniVM
+{
+    param(
+    [Parameter(ValueFromPipeline=$true)][string]$VM,
+    [string]$VMName)
+    process
+    {
+        $url = "https://10.20.4.143/api/virtual_machines/" + $_ + "/clone"
+        $header = @{}
+        $header.Add("Authorization", "Bearer $Token")
+        $header.Add("Accept", "application/json")
+        $header.Add("Content-Type", "application/vnd.simplivity.v1+json")
+        $body = @{}
+        $body.Add("app_consistent", "false")
+        $body.Add("virtual_machine_name", "$VMName")
+        $body = $body | ConvertTo-Json
+        $result = Invoke-RestMethod -Uri $url -Headers $header -Body $body -Method Post
+        return $result.task.id
+    }
+}
+
+# -----------------
+#  MAIN SCRIPT BODY
+# -----------------
+
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+[System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Get-AccessToken
+
+#$vm_id = Get-OmniVM $token "Jon1"
+#$clone_job = Clone-OmniVM $token $vm_id "Jon54321"
+
+$test = Get-OmniVM -VMName Jon1 | Clone-OmniVM -VMName "Jon54321"
+#$test = Get-OmniVM $token "Jon1" | Clone-OmniVM2 -Token $token -VMName "Jon54321"
